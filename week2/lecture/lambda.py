70ad0517e273595d9935e438fa7f3e70f2cb6e78
ju <parrot@localhost.localdomain>
2022-11-30 16:49:20 -0500

    fix

--- week2/lecture/lambda.py
+++
@@ -1,9 +0,0 @@
-people = [
-    {"name": "Harry", "house":"griff"},
-    {"name": "zen", "house":"Griff"},
-    {"name": "her", "house":"slyth"},
-    {"name": "john", "house":"raven"}
-]
-
-people.sort(key=lambda person: person["name"])
-print(people)
